d00e7fa281e2b1bb8c154c14a216803aed1c6b95
ease <easetreychen@gmail.com>
2018-09-19 00:09:50 +0800

    增加公用组件 Footer Header , 增加公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/pages/index">index</router-link> |
-      <router-link to="/pages/login">login</router-link> |
-      <router-link to="/pages/create">create</router-link> |
-      <router-link to="/pages/details">details</router-link>
+    <div id="app">
+        <Header></Header>
+        <main id="main">8888</main>
+        <Footer></Footer>
     </div>
-    <router-view/>
-  </div>
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script>
+    import Header from "./components/common/Header"
+    import Footer from "./components/common/Footer"
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+    export default {
+        name: "App",
+        components: {
+            Header,
+            Footer
+        }
+    }
+</script>
+<style lang="stylus">
+    /*公共样式*/
+    *
+        margin 0
+        padding 0
+        box-sizing border-box
+
+    html,body,#app
+        height:100vh
+    #app {
+        display grid
+        grid-template-columns 12% auto 12%
+        grid-template-rows auto 1fr auto
+        grid-template-areas "header header header"\
+        ".      main  ."\
+        "footer footer footer"
+        border 1px solid red
+    }
+
+    #header
+        grid-area header
+        border 1px solid red
+        padding 0 12%
+
+    #main
+        grid-area main
+        border 1px solid green
+
+    #footer
+        grid-area footer
+        padding-left 12%
+        border 1px solid red
+
 </style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="header">
+        <h1>LET`S SHARE</h1>
+        <span>精品博客汇聚</span>
+        <section>
+            <el-button plain @click="test">朴素按钮</el-button>
+            <el-button plain>朴素按钮</el-button>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        methods: {
+            test() {
+                alert(1)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
