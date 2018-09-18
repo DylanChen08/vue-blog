905c372363425f21bc095f55b56a28236e8e1235
ease <easetreychen@gmail.com>
2018-09-18 15:51:14 +0800

    修复 request.js 登录组件的bug

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import {Message} from 'element-ui'
 
-axios.defaults.baseURL = ' http://blog-server.hunger-valley.com';
+axios.defaults.baseURL = 'http://blog-server.hunger-valley.com';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+axios.defaults.withCredentials = true
 /*
 * 对axios的request进行封装，使用更加方便
 * 传入三个参数:url,请求类型,请求的数据。
@@ -14,7 +14,7 @@ export default function request(url, type = "GET", data = {}) {
     return new Promise((resolve, reject) => {
         let option = {
             url,
-            type
+            method:type
         }
         console.log(option)
         if (type.toLowerCase() === "get") {
@@ -26,14 +26,17 @@ export default function request(url, type = "GET", data = {}) {
         }
         axios(option).then(res => {
             console.log(2)
-            console.log(res)
-            if (res.data === 'ok') {
+            // console.log(res)
+            if (res.data.status === 'ok') {
                 console.log(3)
-                resolve(res.data)
+                resolve(res)
             } else {
                 console.log(4)
-                Message.error(res.data)
-                reject(res.data)
+                console.log(res)
+                console.log(5)
+                Message.error(res.data.msg)
+                console.log(6)
+                reject(res)
             }
         }).catch(e => {
             console.log(e)
