65f0569a61140c7fbfbd1e725b9c811ed871fc02
ease <easetreychen@gmail.com>
2018-09-19 23:41:10 +0800

    定义Header中login和noLogin的样式，重新规划样式的放置

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,20 +1,34 @@
 <template>
-    <div id="header">
-        <h1>LET`S SHARE</h1>
-        <span>精品博客汇聚</span>
-        <section>
-            <el-button plain @click="test">朴素按钮</el-button>
-            <el-button plain>朴素按钮</el-button>
-        </section>
-    </div>
+    <header :class="{'login':isLogin,'no-login':!isLogin}">
+        <!--未登录状态header-->
+        <template v-if="!isLogin">
+            <h1>let`s share</h1>
+            <span>精品博客汇聚</span>
+            <section>
+                <el-button plain @click="test">朴素按钮</el-button>
+                <el-button plain>朴素按钮</el-button>
+            </section>
+        </template>
+        <!--登录状态header-->
+        <template v-if="isLogin">
+            <h1>let`s share</h1>
+            <i class="el-icon-plus"></i>
+            <img class="avatar" src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
+        </template>
+    </header>
 </template>
 
-<style lang="stylus">
-</style>
+
 <script>
+
 
     export default {
         name: "Header",
+        data() {
+            return {
+                isLogin: false
+            }
+        },
         methods: {
             test() {
                 alert(1)
@@ -23,6 +37,6 @@
     }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 
 </style>
