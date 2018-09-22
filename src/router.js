56a108e06b60c79f1aa3d5979c7113f707fea13a
ease <easetreychen@gmail.com>
2018-09-22 10:07:37 +0800

    删除home.vue(不需要的组件)

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,12 +7,12 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-            /*这种写法要从上面import*/
-        },
+        // {
+        //     path: '/',
+        //     name: 'home',
+        //     component: Home
+        //     /*这种写法要从上面import*/
+        // },
         {
             path: '/about',
             name: 'about',
