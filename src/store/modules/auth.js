d861aaa0c458d5fd99e68738756111ef4cd235d6
ease <easetreychen@gmail.com>
2018-09-22 17:50:01 +0800

    完善Vuex中的auth.js

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -15,33 +15,33 @@ const mutation = {
     }
 }
 const actions = {
-    logIn({commit},{username,password}){
-        return auth.logIn({username,password}).then(res=>{
-            commit("setUser",{user:res.data})
-            commit("setLogin",{isLogin:true})
+    logIn({commit}, {username, password}) {
+        return auth.logIn({username, password}).then(res => {
+            commit("setUser", {user: res.data})
+            commit("setLogin", {isLogin: true})
         })
     },
-    async logOut({commit}){
-       await auth.logOut()
-        commit("setUser",null)
-        commit("isLogin",false)
+    async logOut({commit}) {
+        await auth.logOut()
+        commit("setUser", null)
+        commit("isLogin", false)
     },
-    async register({commit},{username,password}){
-        let res = await auth.register({username,password})
-        commit("setUser",{user:res.data})
-        commit("isLogin",{isLogin:true})
+    async register({commit}, {username, password}) {
+        let res = await auth.register({username, password})
+        commit("setUser", {user: res.data})
+        commit("isLogin", {isLogin: true})
     },
-    async checkLogin({commit,state}){
-        if(state.isLogin) return true
-        let res= await auth.getInfo()
-        commit("setLogin",{isLogin:res.isLogin})
-        if(!state.isLogin) return false
-        commit("setUser",{user:res.user})
+    async checkLogin({commit, state}) {
+        if (state.isLogin) return true
+        let res = await auth.getInfo()
+        commit("setLogin", {isLogin: res.isLogin})
+        if (!state.isLogin) return false
+        commit("setUser", {user: res.user})
     }
 }
 
 export default {
-    state ,
+    state,
     getters,
     mutation,
     actions
