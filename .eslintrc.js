5aab1cae1fa0191f60c26388c75f28d1cef1fc4b
DC <easetreychen@gmail.com>
2021-07-13 18:22:57 +0800

    更新配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         'plugin:vue/essential',
         'eslint:recommended'
     ],
+
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
